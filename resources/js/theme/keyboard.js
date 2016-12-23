84ea972e562e036c52631fc8bdc8273fb9c0f6f1
Ryan Thompson <ryan@pyrocms.com>
2016-12-23 00:36:41 -0600

    Correct implementation

--- resources/js/theme/keyboard.js
+++ resources/js/theme/keyboard.js
@@ -1,20 +1,11 @@
 $(function () {
 
-    var delta = 500;
+    var delta = 400;
     var last = 0;
-    var keys = [];
 
     $(document).keydown(function (e) {
 
-        keys[e.keyCode] = true;
-
-        var count = 0;
-
-        for (var i = 0; i < keys.length; i++) {
-            if (keys[i]) count++;
-        }
-
-        if (count == 1 && (e.ctrlKey || e.metaKey)) {
+        if (e.which == 17 || e.which == 91) {
 
             var time = new Date();
 
@@ -27,9 +18,5 @@ $(function () {
 
             last = time;
         }
-    });
-
-    $(document).keyup(function (e) {
-        delete keys[e.keyCode];
     });
 });
