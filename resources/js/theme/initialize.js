3ad0f2d2e9800eb8193737e89305e38c4d67d8ea
Denis Efremov <efremov.a.denis@gmail.com>
2017-03-13 21:31:00 +0300

    Update initialize.js
    Not much nicer than Vanilla))

--- resources/js/theme/initialize.js
+++ resources/js/theme/initialize.js
@@ -1,5 +1,4 @@
 $(function () {
-
     // Add CSRF ajax requests.
     $.ajaxSetup({
         headers: {
@@ -18,4 +17,52 @@ $(function () {
 
     // Flush the footer to the bottom.
     $('#main').css('min-height', $(window).height() - $('#brand').outerHeight() - $('#footer').outerHeight());
+
+    // Fix top form controls when scrolling.
+    // $('form .controls:first-of-type').affix({
+    //     offset: {
+    //         top: 120
+    //     }
+    // });
+
+    var $table = $('table.table'), scrollTop, output;
+
+    if ($table.length) {
+        var
+            $topBar = $('#topbar'),
+            $thead = $table.find('thead'),
+            $tbody = $table.find('tbody'),
+
+            topBarHeight = $topBar.height(),
+            tableWidth = $table.width(),
+            tableTop = $table.closest('.card').offset().top,
+
+            tableColsSizes = (function ($tbody) {
+                output = [];
+
+                $tbody.find('tr:first-child td').each(function (index, el) {
+                    output.push($(el).width());
+                });
+
+                return output;
+            })($tbody);
+
+        $thead.css({ backgroundColor: '#fff', width: tableWidth + 'px' });
+
+        $(window).on('scroll', function (event) {
+            scrollTop = $(event.target.body).scrollTop();
+
+            $thead.find('th').each(function (index, el) {
+                $(el).width(tableColsSizes[index] + 'px');
+            });
+
+            if (scrollTop > tableTop - topBarHeight) {
+                $thead.css({ position: 'fixed', top: topBarHeight + 'px' });
+                $table.css({ marginTop: $thead.height() + 'px' });
+            } else {
+                $thead.css({ position: 'relative', top: '0px' });
+                $table.css({ marginTop: '0' });
+            }
+        });
+    }
 });
